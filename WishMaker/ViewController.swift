935dc37f2c019bb72f5cecc96e48e9904e7a83ee
rphaet0n <phaet0n@mrphaet0ns-Mac-Pro.local>
2017-04-23 16:56:52 +0300

    Start!

---
+++ WishMaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WishMaker
+//
+//  Created by mr.phaet0n on 4/23/17.
+//  Copyright © 2017 mmcs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
