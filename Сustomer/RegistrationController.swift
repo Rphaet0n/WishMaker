a6075a5eaafa7ff12a09d978d4005b2af5da8fdc
user161rus <karnovan95@mail.ru>
2017-04-26 17:46:42 +0300

    add some interface

---
+++ Сustomer/RegistrationController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Сustomer
+//
+//  Created by maxik on 23.04.17.
+//  Copyright © 2017 Company. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
