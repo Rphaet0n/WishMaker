5c8dfb104133c317641372b87fc81b209967d443
ardashes <phaet0n@inbox.ru>
2017-05-03 17:51:30 +0300

    Added User and Order Models

---
+++ Сustomer/OrderModel.swift
@@ -0,0 +1,47 @@
+//
+//  OrderModel.swift
+//  WishMaker
+//
+//  Created by student on 03.05.17.
+//  Copyright © 2017 Company. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class OrderModel: NSObject {
+    
+    //properties
+    var idOrder: Int?
+    var title: String?
+    var desc: String?
+    var price: Int?
+    var address: String?
+    var latitude: Double?
+    var longitude: Double?
+    var images: [UIImage] = []
+    var startDate: Date?
+    var endDate: Date?
+    
+    //empty constructor
+    override init()
+    {
+    }
+    
+    //construct with @idOrder, @title, @city, and @price parameters
+    init(idOrder: Int, title: String, address: String, price: Int, startDate: Date) {
+        self.idOrder = idOrder
+        self.title = title
+        self.address = address
+        self.price = price
+        self.startDate = startDate
+    }
+    
+    //prints object's current state
+    override var description: String {
+        return "idOrder: \(idOrder), title: \(title), address: \(address), price: \(price), startDate: \(startDate)"
+        
+    }
+    
+}
---
+++ Сustomer/UserModel.swift
@@ -0,0 +1,41 @@
+//
+//  UserModel.swift
+//  WishMaker
+//
+//  Created by student on 03.05.17.
+//  Copyright © 2017 Company. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class UserModel: NSObject {
+    
+    //properties
+    var idUser: Int?
+    var fullName: String?
+    var mobileNo: String?
+    var rating: Double?
+    var avatar: UIImage?
+    
+    //empty constructor
+    override init()
+    {
+    }
+    
+    //construct with @idUser, @fullName, @mobileNo, @rating, and @avatar parameters
+    init(idUser: Int, fullName: String, mobileNo: String, rating: Double, avatar: UIImage? = nil) {
+        self.idUser = idUser
+        self.fullName = fullName
+        self.mobileNo = mobileNo
+        self.rating = rating
+        self.avatar = avatar
+    }
+    
+    //prints object's current state
+    override var description: String {
+        return "idUser: \(idUser), fullName: \(fullName), mobileNo: \(mobileNo), rating: \(rating)"
+    }
+    
+}
