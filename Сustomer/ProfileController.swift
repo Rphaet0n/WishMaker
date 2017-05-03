eb4945bbebfc0bd3243c5e8b700032eca7f7bf78
user161rus <karnovan95@mail.ru>
2017-05-03 18:57:31 +0300

    Some featuries

--- Сustomer/ProfileController.swift
+++ Сustomer/ProfileController.swift
@@ -4,19 +4,70 @@
 //
 //  Created by maxik on 23.04.17.
 //  Copyright © 2017 Company. All rights reserved.
-//
+//https://makeapppie.com/2016/06/28/how-to-use-uiimagepickercontroller-for-a-camera-and-photo-library-in-swift-3-0/
 
 import UIKit
 
-class ProfileController: UIViewController {
+class ProfileController: UIViewController,
+    UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
+    @IBOutlet weak var avatarView: UIImageView!
     
-    @IBAction func photoFromLibrary(_ sender: UIButton) {
+    @IBAction func photoFromLibrary(_ sender: UIBarButtonItem) {
+        picker.allowsEditing = false
+        picker.sourceType = .photoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        picker.modalPresentationStyle = .popover
+        present(picker, animated: true, completion: nil)
+        picker.popoverPresentationController?.barButtonItem = sender
     }
+    
+    
+    @IBAction func shootPhoto(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.allowsEditing = false
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            picker.cameraCaptureMode = .photo
+            picker.modalPresentationStyle = .fullScreen
+            present(picker,animated: true,completion: nil)
+        } else {
+            noCamera()
+        }
+    }
+    func noCamera(){
+        let alertVC = UIAlertController(
+            title: "No Camera",
+            message: "Sorry, this device has no camera",
+            preferredStyle: .alert)
+        let okAction = UIAlertAction(
+            title: "OK",
+            style:.default,
+            handler: nil)
+        alertVC.addAction(okAction)
+        present(
+            alertVC,
+            animated: true,
+            completion: nil)
+    }
+    
+    var picker = UIImagePickerController()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        picker.delegate = self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+        avatarView.contentMode = .scaleAspectFit //3
+        avatarView.image = chosenImage //4
+        dismiss(animated:true, completion: nil) //5
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
     }
     
     override func didReceiveMemoryWarning() {
